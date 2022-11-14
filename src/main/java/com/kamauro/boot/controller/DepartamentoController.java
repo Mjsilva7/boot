f1765e9234ee71e755b4391df095b791fb81de19
Mjsilva7 <mj.silva7@gmail.com>
2022-11-14 15:49:13 -0300

    Refatoração

--- src/main/java/com/kamauro/boot/controller/DepartamentoController.java
+++ src/main/java/com/kamauro/boot/controller/DepartamentoController.java
@@ -1,21 +1,59 @@
 package com.kamauro.boot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kamauro.boot.model.Departamento;
+import com.kamauro.boot.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 
+    @Autowired
+    private DepartamentoService service;
+
     @GetMapping("/cadastrar")
-    public String cadastrar() {
+    public String cadastrar(Departamento departamento) {
         return "/departamento/cadastro";
     }
 
     @GetMapping("/listar")
-    public String listar() {
+    public String listar(ModelMap model) {
+        model.addAttribute("departamentos", service.buscarTodos());
+        
         return "/departamento/lista";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(Departamento departamento) {
+        service.salvar(departamento);
+        return "redirect:/departamentos/cadastrar";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String formEditar(@PathVariable("id") Long id, ModelMap model) {
+        model.addAttribute("departamento", service.buscarPorId(id));
+        return "/departamento/cadastro";
+    }
+
+    @PostMapping("/editar")
+    public String editar(Departamento departamento) {
+        service.editar(departamento);
+        return "redirect:/departamentos/listar";
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluir(@PathVariable("id") Long id, ModelMap model) {
+        if(!service.departamentoTemCargos(id)) {
+            service.excluir(id);
+        }
+        return listar(model);
     }
     
 }
